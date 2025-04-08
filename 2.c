f8464fa407fccf464d057c59ec083a0c9b8282f3
nikhil-barve1 <barvenikhil14@gmail.com>
2025-04-08 14:04:31 +0530

    learned data types and format specifiers

---
+++ 2.c
@@ -0,0 +1,39 @@
+// WAP to print hello world
+#include <stdio.h>
+
+int main()
+{
+    // printf("Value = %d", 5);
+    // printf("\nValue = %f", 98.5000);
+    // printf("\nValue = %.2f", 98.5000);
+
+    int no1;
+    float f;
+    double d;
+    char ch;
+
+    // size is always in integer value.
+    printf("Int size = %d", sizeof(int));
+    printf("\nFloat size = %d", sizeof(float));
+    printf("\nChar size = %d", sizeof(char));
+    printf("\nDouble size = %d", sizeof(double));
+
+    // printf("Enter No : ");
+    // scanf("%d", &no1);
+
+    // printf("Enter Float No : ");
+    // scanf("%f", &f);
+
+    // printf("Enter Double No : ");
+    // scanf("%g", &d);
+
+    // printf("Enter Char : ");
+    // scanf(" %c", &ch);
+
+    // printf("\nInteger : %d", no1);
+    // printf("\nFloat : %.3f", f);
+    // printf("\nDouble : %.2g", d);
+    // printf("\nChar : %c", ch);
+
+    return 0;
+}
---
+++ Firstprog.c
@@ -0,0 +1,12 @@
+// WAP to print hello world
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world");
+    printf("\n Hello world");
+    printf("\t Hello world");
+    printf("\nHello \" world");
+
+    return 0;
+}
